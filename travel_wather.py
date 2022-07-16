18bbc1242e74ca4c1b60a4f73cff3f995ae58a3f
Fay <fay.tseng@gmail.com>
2022-07-16 18:30:42 +0800

    Add files via upload

--- travel_wather.py
+++ travel_wather.py
@@ -1,13 +1,12 @@
 # 1. Library imports
-pip install requests
-
+#pip install requests
 
 # 2. Introduce the application
 print("\t\t歡迎進入旅遊地點天氣預測!\n\n")
 print("請輸入旅遊城市名稱，輸入完後請按下按鈕! Let's Go!\n\n")
 
 # 3. input of the city name from the user using the code
-city_name = input("輸入旅遊城市名稱 : 例如Tokyo ")
+city_name = input("輸入旅遊城市名稱 : (例如 Tokyo) ")
 
 # 4. importing the requests module
 import requests
