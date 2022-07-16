a614b12ca669dd7a87b0fb7b256de1aa4626fc47
Fay <fay.tseng@gmail.com>
2022-07-16 20:59:02 +0800

    Add files via upload

---
+++ travel_weather_input.py
@@ -0,0 +1,24 @@
+# 1. Library imports
+#pip install requests
+
+# 2. Introduce the application
+print("\t\t歡迎進入旅遊地點天氣預測!\n\n")
+print("本次要旅遊城市及地點是歐洲克羅埃西亞，輸入完後請按下按鈕! Let's Go!\n\n")
+
+# 3. input of the city name from the user using the code
+city_name = input("輸入旅遊城市名稱 : (例如 Tokyo) ")
+
+# 4. importing the requests module
+import requests
+
+# 5. We will create a function that will take the name of the city the user enters and prints the report for us. Look at the code of the function below.
+def Gen_report(C):
+    print("旅遊城市 : " + C)
+    url = 'https://wttr.in/{}'.format(C)
+    try:
+        data = requests.get(url)
+        T = data.text
+    except:
+        T = "Error Occurred"
+    print(T)
+Gen_report(city_name)
